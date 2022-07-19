d9ea31e1dc5c65258214abed64ff04e53b3312d4
Juvencio Linhares <jlinhares@cpqi.com>
2022-07-19 15:58:11 -0300

    Interface Comparable

---
+++ InterfaceComparable/InterfaceComparable/Entities/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace InterfaceComparable.Entities
+{
+    internal class Employee : IComparable//implemento IComparable para que o método  list.Sort(); possa comparar e ordenar.
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployye)//arquivo que vai ser recebido do dodumento .txt
+        {
+            string[] vect = csvEmployye.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        //metodo de IComparable que  compara um objeto passado no parametro
+        public int CompareTo(object obj)//compara 2 obj e retorna um inteiro: -1: menor, 0 :igual, 1: maior;
+        {
+            //comparar um funcionario com outro
+
+            //escolho comparar por nome
+            if (!(obj is Employee))//comparo se o obj do arguemento é do tipo employye
+            {
+                throw new ArgumentException("Comparer error: argument is not an Employee");
+            }
+
+            Employee other = obj as Employee;// downcast(o employee other recebe o obj do argumento)
+
+            return Name.CompareTo(other.Name);//o tipo string ja implementa comparable
+
+        }
+    }
+}
---
+++ InterfaceComparable/InterfaceComparable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using InterfaceComparable.Entities;
+
+namespace InterfaceComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\testeInterfaceIComparable\in.txt";
+            try
+            {
+                //ler e guardar na lista:
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));//novo funcionario lendo uma linha do arquivo .txt
+                    }
+
+                    //ordenar a lista:
+                    list.Sort();//faz o uso da interface IComparable, ou seja, pra ordenar tem que comparar
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
